82c0129fb4ad08f786cfac9d0df7cb07e88e3eb5
Anton <grishin.ant.a@gmail.com>
2021-12-12 13:28:47 +0500

    Main переписан на функциональный компонент с хуками

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,28 +1,16 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Movies from '../components/Movies';
 import Preloader from '../components/Preloader';
 import Search from '../components/Search';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-class Main extends Component {
-  state = {
-    movies: [],
-    loading: true,
-  };
+const Main = () => {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  componentDidMount() {
-    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
-      .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }))
-      .catch((err) => {
-        console.log(err);
-        this.setState({ loading: false });
-      });
-  }
-
-  updateMoviesFromSearch = (str, type = 'all') => {
-    this.setState({ loading: true });
+  const updateMoviesFromSearch = (str, type = 'all') => {
+    setLoading(true);
     let link = '';
     if (type === 'all') {
       link = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}`;
@@ -31,22 +19,35 @@ class Main extends Component {
     }
     fetch(link)
       .then((response) => response.json())
-      .then((data) => this.setState({ movies: data.Search, loading: false }))
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      })
       .catch((err) => {
         console.log(err);
-        this.setState({ loading: false });
+        setLoading(false);
       });
   };
 
-  render() {
-    const { movies, loading } = this.state;
-    return (
-      <main className="container content">
-        <Search updateMoviesFromSearch={this.updateMoviesFromSearch} />
-        {loading ? <Preloader /> : <Movies movies={movies} />}
-      </main>
-    );
-  }
-}
+  useEffect(() => {
+    fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data.Search);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <main className="container content">
+      <Search updateMoviesFromSearch={updateMoviesFromSearch} />
+      {loading ? <Preloader /> : <Movies movies={movies} />}
+    </main>
+  );
+};
 
 export default Main;
